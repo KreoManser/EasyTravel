1cbf5ee283ea0ebe66c9ea4578d3cec5afaaa90e
KreoManser <sherba.ru@list.ru>
2022-07-09 15:10:40 +0300

    merge develop_Sergey with develop_Roman

---
+++ EasyTravel/ViewControllers/PopTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PopTableViewCell.swift
+//  EasyTravel
+//
+//  Created by Сергей Бабич on 09.07.2022.
+//
+
+import UIKit
+
+class PopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- EasyTravel/ViewControllers/PopTableViewController.swift
+++ EasyTravel/ViewControllers/PopTableViewController.swift
@@ -8,21 +8,40 @@
 import UIKit
 
 class PopTableViewController: UITableViewController {
+    
+    let tripsArray = [
+                "Test 1",
+                "Test 1",
+                "Test 3"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.isScrollEnabled = false
+    }
+    
+    override func viewWillLayoutSubviews() {
+        preferredContentSize = CGSize(width: 250, height: tableView.contentSize.height)
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tripsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let textData = tripsArray[indexPath.row]
+        cell.textLabel?.text = textData
+        return cell
     }
 }
