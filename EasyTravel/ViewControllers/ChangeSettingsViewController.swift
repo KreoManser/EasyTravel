c733ec433bd2193ab44b9fb3f2bdab56e7c117a2
Vadim <xenon369@mail.ru>
2022-07-09 18:23:58 +0300

    final edition

--- EasyTravel/ViewControllers/ChangeSettingsViewController.swift
+++ EasyTravel/ViewControllers/ChangeSettingsViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+<<<<<<< Updated upstream
 enum GenderType: String {
     case male = "Мужчина"
     case female = "Женщина"
@@ -20,6 +21,11 @@ class UserModel {
         self.name = name
         self.gender = gender
     }
+=======
+
+protocol ChangeUserInfo: AnyObject {
+    func changeData(user: User)
+>>>>>>> Stashed changes
 }
 
 class ChangeSettingsViewController: UIViewController {
@@ -29,11 +35,16 @@ class ChangeSettingsViewController: UIViewController {
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     var pickedGender: GenderType?
+<<<<<<< Updated upstream
+=======
+    weak var delegate: ChangeUserInfo?
+>>>>>>> Stashed changes
         
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+<<<<<<< Updated upstream
     @IBAction func saveButtonDidTap(_ sender: Any) {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
@@ -57,6 +68,40 @@ class ChangeSettingsViewController: UIViewController {
 //        dismiss(animated: true)
         
         
+=======
+    override func viewWillAppear(_ animated: Bool) {
+    }
+    
+    @IBAction func saveButtonDidTap(_ sender: Any) {
+        if nameTextField.text == "" {
+            checker()
+        } else {
+            switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                pickedGender = .male
+            case 1:
+                pickedGender = .female
+            default:
+                break
+            }
+            
+            guard let pickedGender = pickedGender else { return }
+            
+            UserSettings.userName = nameTextField.text
+            UserSettings.userGender = pickedGender.rawValue
+            
+            let user = User(name: UserSettings.userName, gender: UserSettings.userGender)
+            delegate?.changeData(user: user)
+            dismiss(animated: true)
+        }
+    }
+
+    func checker() {
+        let alert = UIAlertController(title: "Ошибка", message: "Вы не ввели имя!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+>>>>>>> Stashed changes
     }
 }
 
