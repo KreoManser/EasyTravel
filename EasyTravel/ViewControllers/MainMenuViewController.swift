98af20dfd29a93f28241d0e80d2103e91ceeb6c8
KreoManser <sherba.ru@list.ru>
2022-07-09 13:43:43 +0300

    Replace code

--- EasyTravel/ViewControllers/MainMenuViewController.swift
+++ EasyTravel/ViewControllers/MainMenuViewController.swift
@@ -137,26 +137,3 @@ extension MainMenuViewController: UICollectionViewDelegate, UICollectionViewData
         }
     }
 }
-
-// MARK: - TotalMoneyCollectionViewCell
-
-class TotalMoneyCollectionViewCell: UICollectionViewCell {
-    
-    // MARK: - IBOutlets
-    
-    @IBOutlet weak var totalBudgetLabel: UILabel!
-    
-    // MARK: - Private Properties
-    
-    var totalBudgetMoney = 0
-    
-    // MARK: - IBActions
-    
-    // MARK: - Methods
-    
-    func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let changeBudgetVC = segue.destination as? ChangeBudgetViewController else { return }
-        
-        changeBudgetVC.change = totalBudgetLabel.text
-    }
-}
--- EasyTravel/ViewControllers/TotalMoneyCollectionViewCell.swift
+++ EasyTravel/ViewControllers/TotalMoneyCollectionViewCell.swift
@@ -5,30 +5,27 @@
 //  Created by Рустам Шайхинуров on 08.07.2022.
 //
 
-//import UIKit
-//
-//// MARK: - TotalMoneyCollectionViewCell
-//
-//class TotalMoneyCollectionViewCell: UICollectionViewCell {
-//    
-//    // MARK: - IBOutlets
-//    
-//    @IBOutlet weak var totalBudgetLabel: UILabel!
-//    @IBOutlet weak var addMoneyButton: UIButton!
-//    
-//    // MARK: - Private Properties
-//    
-//    var totalBudgetMoney = 0
-//    
-//    // MARK: - IBActions
-//    
-//    @IBAction func addMoneyButtonDidTap(_ sender: Any) {
-////        totalBudgetMoney += 100
-////        totalBudgetLabel.text = String(totalBudgetMoney)
-////
-////        let storyboard = UIStoryboard(name: "ChangeBudget", bundle: nil)
-////        guard let changeBudgetVC = storyboard.instantiateViewController(withIdentifier: "ChangeBudgetViewController") as? ChangeBudgetViewController else { return }
-////
-////        present(changeBudgetVC, animated: true)
-//    }
-//}
+import UIKit
+
+// MARK: - TotalMoneyCollectionViewCell
+
+class TotalMoneyCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var totalBudgetLabel: UILabel!
+    
+    // MARK: - Private Properties
+    
+    var totalBudgetMoney = 0
+    
+    // MARK: - IBActions
+    
+    // MARK: - Methods
+    
+    func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let changeBudgetVC = segue.destination as? ChangeBudgetViewController else { return }
+        
+        changeBudgetVC.change = totalBudgetLabel.text
+    }
+}
