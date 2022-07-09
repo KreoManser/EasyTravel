9f038ca2bf306fc2e8fb78b92240960274ef7eca
Vadim <xenon369@mail.ru>
2022-07-09 18:49:48 +0300

    Revert "final edition"
    This reverts commit c733ec433bd2193ab44b9fb3f2bdab56e7c117a2.

--- EasyTravel/ViewControllers/ChangeSettingsViewController.swift
+++ EasyTravel/ViewControllers/ChangeSettingsViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-<<<<<<< Updated upstream
 enum GenderType: String {
     case male = "Мужчина"
     case female = "Женщина"
@@ -21,11 +20,6 @@ class UserModel {
         self.name = name
         self.gender = gender
     }
-=======
-
-protocol ChangeUserInfo: AnyObject {
-    func changeData(user: User)
->>>>>>> Stashed changes
 }
 
 class ChangeSettingsViewController: UIViewController {
@@ -35,16 +29,11 @@ class ChangeSettingsViewController: UIViewController {
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     var pickedGender: GenderType?
-<<<<<<< Updated upstream
-=======
-    weak var delegate: ChangeUserInfo?
->>>>>>> Stashed changes
         
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-<<<<<<< Updated upstream
     @IBAction func saveButtonDidTap(_ sender: Any) {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
@@ -68,40 +57,6 @@ class ChangeSettingsViewController: UIViewController {
 //        dismiss(animated: true)
         
         
-=======
-    override func viewWillAppear(_ animated: Bool) {
-    }
-    
-    @IBAction func saveButtonDidTap(_ sender: Any) {
-        if nameTextField.text == "" {
-            checker()
-        } else {
-            switch segmentedControl.selectedSegmentIndex {
-            case 0:
-                pickedGender = .male
-            case 1:
-                pickedGender = .female
-            default:
-                break
-            }
-            
-            guard let pickedGender = pickedGender else { return }
-            
-            UserSettings.userName = nameTextField.text
-            UserSettings.userGender = pickedGender.rawValue
-            
-            let user = User(name: UserSettings.userName, gender: UserSettings.userGender)
-            delegate?.changeData(user: user)
-            dismiss(animated: true)
-        }
-    }
-
-    func checker() {
-        let alert = UIAlertController(title: "Ошибка", message: "Вы не ввели имя!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        alert.addAction(action)
-        self.present(alert, animated: true, completion: nil)
->>>>>>> Stashed changes
     }
 }
 
--- EasyTravel/ViewControllers/SettingsViewController.swift
+++ EasyTravel/ViewControllers/SettingsViewController.swift
@@ -7,22 +7,15 @@
 
 import UIKit
 
-<<<<<<< Updated upstream
 class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var TripImageView: UIImageView!
-=======
-class SettingsViewController: UIViewController {
-    
-    
->>>>>>> Stashed changes
     @IBOutlet weak var genderLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var changeSettingsButton: UIButton!
     @IBOutlet weak var rateButton: UIButton!
     @IBOutlet weak var aboutButton: UIButton!
-<<<<<<< Updated upstream
     
     var settings: [settingModel] = []
     
@@ -34,34 +27,15 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-=======
-    @IBOutlet weak var themeSwitch: UISwitch!
-    @IBOutlet weak var themeLabel: UILabel!
-    
-    // MARK: - View life cyrcle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUpUI()
-        saveData()
-    }
-
-    
-    private func setUpUI() {
->>>>>>> Stashed changes
         changeSettingsButton.layer.shadowColor = UIColor.black.cgColor
         changeSettingsButton.layer.shadowOpacity = 0.6
         changeSettingsButton.layer.shadowRadius = 10
         changeSettingsButton.layer.shadowOffset = CGSize(width: 0, height: 0)
-<<<<<<< Updated upstream
         
-=======
->>>>>>> Stashed changes
         rateButton.layer.shadowColor = UIColor.black.cgColor
         rateButton.layer.shadowOpacity = 0.6
         rateButton.layer.shadowRadius = 10
         rateButton.layer.shadowOffset = CGSize(width: 0, height: 0)
-<<<<<<< Updated upstream
         
         appendSettings()
         
@@ -116,55 +90,3 @@ class SettingsViewController: UIViewController {
 
 
 
-=======
-        themeSwitch.isOn = false
-        themeSwitch.onTintColor = UIColor(named: "accentColor-1")
-    }
-    
-    func saveData() {
-        if UserSettings.userName == nil {
-            UserSettings.userName = "Username"
-        }
-        if UserSettings.userGender == nil {
-            UserSettings.userGender = "Пол: -"
-        } else {
-            genderLabel.text = "Пол: \(UserSettings.userGender!)"
-        }
-        usernameLabel.text = UserSettings.userName
-    }
-    
-    // MARK: - Actions
-    
-    @IBAction func notificationButtonDidTap(_ sender: Any) {
-        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else { return }
-        UIApplication.shared.open(settingsUrl)
-    }
-    
-    @IBAction func contactDevelopers(_ sender: Any) {
-        if let url = URL(string: "https://github.com/KreoManser/EasyTravel") {
-            if UIApplication.shared.canOpenURL(url) {
-                UIApplication.shared.open(url, options: [:])
-            }
-        }
-    }
-    
-    @IBAction func changeSettingsButtonDidTap(_ sender: Any) {
-        guard let changeSettingsVC = storyboard?.instantiateViewController(withIdentifier: "ChangeSettingsViewController") as? ChangeSettingsViewController else { return }
-        changeSettingsVC.delegate = self
-        present(changeSettingsVC, animated: true)
-    }
-    
-    @IBAction func aboutButtonDidTap(_ sender: Any) {
-        guard let aboutVC = storyboard?.instantiateViewController(withIdentifier: "AboutViewController") as? AboutViewController else { return }
-        present(aboutVC, animated: true)
-    }
-}
-
-// MARK: - ChangeUserInfo
-extension SettingsViewController: ChangeUserInfo {
-    func changeData(user: User) {
-        usernameLabel.text = user.name
-        genderLabel.text = "Пол: \(user.gender)"
-    }
-}
->>>>>>> Stashed changes
