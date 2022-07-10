7313a16d597d73ed9144985359cac5f0b79d62ad
TimFich <felinooper@gmail.com>
2022-07-10 17:52:34 +0300

    mergge conflict

---
+++ EasyTravel/ViewControllers/ShowStoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowStoryViewController.swift
+//  EasyTravel
+//
+//  Created by Тимур Миргалиев on 08.07.2022.
+//
+
+import UIKit
+
+class ShowStoryViewController: UIViewController {
+
+    @IBOutlet weak var imageBackgroundView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
