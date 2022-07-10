e046673631e8f82a7639f6c49a03a18c100cc563
Sergey Babich <88515146+KreoManser@users.noreply.github.com>
2022-07-10 18:07:07 +0300

    Add files via upload

---
+++ EasyTravel/Models/PackagePlan.swift
@@ -0,0 +1,46 @@
+//
+//  PackagePlan.swift
+//  EasyTravel
+//
+//  Created by Сергей Бабич on 05.07.2022.
+//
+
+import Foundation
+import UIKit
+
+enum Package {
+    case trip
+    case visit
+    case hikking
+}
+
+struct Plan {
+    let image: UIImage?
+    let title: String
+    let package: Package
+    let description: String
+
+    
+    static func getPlan() -> [Plan] {
+        [
+            Plan(
+                image: UIImage(named: "storiesImage"),
+                title: "Поход",
+                package: .hikking,
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            ),
+            Plan(
+                image: UIImage(named: "Trip"),
+                title: "Путешествие",
+                package: .trip,
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            ),
+            Plan(
+                image: UIImage(named: "Visit"),
+                title: "Визит",
+                package: .visit,
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            ),
+        ]
+    }
+}
---
+++ EasyTravel/Models/ProductModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductModel.swift
+//  EasyTravel
+//
+//  Created by Тимур Миргалиев on 08.07.2022.
+//
+
+import Foundation
+import UIKit
+
+struct ProductModel {
+    var label: String
+}
