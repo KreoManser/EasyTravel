65103cf951152f067ca30c13f0385efd1cc3b173
KreoManser <sherba.ru@list.ru>
2022-07-11 19:07:10 +0300

    Minor fixed

--- EasyTravel/Models/PackagePlan.swift
+++ EasyTravel/Models/PackagePlan.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import UIKit
 
 let packageList = ["trip", "visit", "hikking"]
 
--- EasyTravel/Models/ProductModel.swift
+++ EasyTravel/Models/ProductModel.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import UIKit
 
 struct ProductModel {
     var label: String
